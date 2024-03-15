1db2140fc2df8c7df1e551650a2173d1757e8845
Wes Hinsley <w.hinsley@imperial.ac.uk>
2024-03-15 17:26:03 +0000

    Basic test project

---
+++ hipercow-api-unit-tests/DideConstantsTests.cs
@@ -0,0 +1,32 @@
+﻿using Hipercow_api;
+
+namespace hipercow_api_unit_tests
+{
+    public class DideConstantsTest
+    {
+        [Fact]
+        public void GetDideClusters_works()
+        {
+            List<string> res = DideConstants.GetDideClusters();
+            Assert.Single(res);
+            Assert.Contains("wpia-hn", res);
+        }
+
+        [Fact]
+        public void GetQueues_works()
+        {
+            List<string> res = DideConstants.GetQueues("wpia-hn");
+            Assert.Equal(2, res.Count);
+            Assert.Contains("AllNodes", res);
+            Assert.Contains("Training", res);
+            Assert.Empty(DideConstants.GetQueues("turnip"));
+        }
+
+        [Fact]
+        public void GetDefaultQueue_works()
+        {
+            Assert.Equal("AllNodes", DideConstants.GetDefaultQueue("wpia-hn"));
+            Assert.Empty(DideConstants.GetDefaultQueue("turnip"));
+        }
+    }
+}
